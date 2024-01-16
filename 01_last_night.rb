b2b4f957aa0e6045e8728eb107b066c16a9757e6
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-16 23:26:12 +0100

    Start the Lecture w/ A Little Practice

---
+++ 01_last_night.rb
@@ -0,0 +1,15 @@
+# Start with a 5-minutes rehearsal on:
+# Data types: String, Integer, Float, Booleans
+# Variables & Methods 🍺
+
+# String
+
+# Integer
+
+# Float
+
+# Booleans
+
+# Variables
+
+# Methods >>>>> Reusable Code
