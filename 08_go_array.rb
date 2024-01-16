9f53a9ab0113e3e97d310fdae39099f7100a8ca3
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-16 23:37:24 +0100

    Build a Program To Finally Go Array

---
+++ 08_go_array.rb
@@ -0,0 +1,14 @@
+# Array
+numbers = []
+pokemons = []
+puts "Welcome to Batch 1565! 👋"
+students_of_1565 = ["Alfred", "Roi", "Viorel", "Carsten", "Romain", "Kalek", "Tarik", "Charlotte", "Antal", "Ben", "Inès", "Valerija", "Dinusha", "Marcus", "Katherine", "Sebastian", "Erwan", "Sidney", "Martin", "Jérôme", "Dominic", "Ana", "Carl", "Alfonso"]
+
+p students_of_1565.count
+sleep(1)
+
+# Index 0, 1, 2, 3
+
+students_of_1565[25] = "Adam"
+
+puts "What?? There is a new student??? The new student of Batch #1565 is 🥁🥁🥁 #{students_of_1565[-1]} 🥁🥁🥁"
