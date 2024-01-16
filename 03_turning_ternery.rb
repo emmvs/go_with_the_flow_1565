fdbfbdde5a591587c872dea4cfdbf5db5f3a6887
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-16 23:27:59 +0100

    Turning The Ternery

---
+++ 03_turning_ternery.rb
@@ -0,0 +1,13 @@
+# Ternary operator
+puts "heads or tails? 🪙"
+choice = gets.chomp
+coin = ["heads", "tails"].sample
+
+# if choice == coin
+#   result = "winner winner chicken dinner"
+# else
+#   result = "loooser"
+# end
+
+result = (choice == coin) ? "winner winner chicken dinner" : "loooser"
+puts "#{result}, that was #{coin}"
