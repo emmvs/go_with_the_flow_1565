67ff136b79f6772179a32441fbbf5cce75c4e29e
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-16 23:43:57 +0100

    Build a Program For The Little Baby Devs

--- 06_only_for_you.rb
+++ 06_only_for_you.rb
@@ -2,9 +2,13 @@
 number_of_students = 0
 students_of_1565 = ["Alfred", "Roi", "Viorel", "Carsten", "Romain", "Kalek", "Tarik", "Charlotte", "Antal", "Ben", "Inès", "Valerija", "Dinusha", "Marcus", "Katherine", "Sebastian", "Erwan", "Sidney", "Martin", "Jérôme", "Dominic", "Ana", "Carl", "Alfonso"]
 
+puts "📣 Here are all the Little Baby Devs of Batch 1565 🍼:"
+
+# Let's use a fun for loop to call out each student's name
 for student in students_of_1565
-  puts student
+  puts "👶 #{student}"
   number_of_students += 1
+  sleep(1) # Pause for suspense!
 end
 
-puts "We've got #{number_of_students} little dev babies 🍼"
+puts "We've got #{number_of_students} little baby devs 🍼 all present and ready to code!"
