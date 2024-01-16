6290de2c0713b4948c865e0ae04ac41ebf5febd8
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-16 23:31:58 +0100

    For you, Ill Try To Explain

---
+++ 06_only_for_you.rb
@@ -0,0 +1,10 @@
+# For Loop
+number_of_students = 0
+students_of_1565 = ["Alfred", "Roi", "Viorel", "Carsten", "Romain", "Kalek", "Tarik", "Charlotte", "Antal", "Ben", "Inès", "Valerija", "Dinusha", "Marcus", "Katherine", "Sebastian", "Erwan", "Sidney", "Martin", "Jérôme", "Dominic", "Ana", "Carl", "Alfonso"]
+
+for student in students_of_1565
+  puts student
+  number_of_students += 1
+end
+
+puts "We've got #{number_of_students} little dev babies 🍼"
